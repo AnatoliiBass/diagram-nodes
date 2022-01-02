14ce55bdb9492b905efb4fc7082a4b2d4c69cad5
AnatoliiBass <bass_1304@ukr.net>
2022-01-02 19:38:45 +0200

    fix click-bug

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,29 @@ import DiagramContainer from './Diagram/DiagramContainer'
 import style from './Main.module.css'
 
 const Main = ({ setCurrentDiagram }) => {
-
    const handlerFunc = (e) => {
-      if (e.target.className === 'css-qcxco2') {
-         setCurrentDiagram(e.target.parentNode.nextElementSibling.outerText)
-      } else {
-         setCurrentDiagram(e.target.textContent)
+      switch (e.target.className) {
+         case 'css-qcxco2': {
+            setCurrentDiagram(e.target?.parentNode?.nextElementSibling?.outerText)
+            break
+         }
+         case 'css-14ytlrw': {
+            setCurrentDiagram(e.target?.childNodes[0]?.childNodes[0]?.childNodes[1]?.childNodes[0]?.textContent ??
+               e.target?.childNodes[1]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.textContent)
+            break
+         }
+         case 'css-mtwhol': {
+            setCurrentDiagram(e.target?.outerText)
+            break
+         }
+         case 'css-11bc0l7': {
+            setCurrentDiagram(e.target?.parentNode?.nextElementSibling?.outerText ??
+               e.target?.parentNode?.parentNode?.parentNode?.previousSibling?.childNodes[0]?.childNodes[1]?.childNodes[0]?.textContent)
+            break
+         }
+         default: {
+            break
+         }
       }
    }
    return (
